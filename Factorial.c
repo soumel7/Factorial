d15a44a53f8390832565e9cc91ba92ba1392f9b7
Soumel Zutshi <soumelzutshi@gmail.com>
2018-10-14 13:48:41 +0530

    Factorial program by recursion

--- Factorial.c
+++ Factorial.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
-#include<conio.h>
-void main()
+#include<stdlib.h>
+int fact(int);//Funtion declaration
+/*Factorial will be calculated by recursion*/
+int main()
 {
-	int num,res;
-	clrscr();
-	scanf("Enter the Number %d",&num);
-	res=fact(n);
-	printf("Factorial : %d",res);
-	getch();
+	int no,ans;
+	printf("Enter a number:");
+	scanf("%d",&no);
+	ans=fact(no);//Function calling
+	printf("Factorial is %d",ans);
+}
+int fact(int x)//Function defining
 {
+	int f;
+	if(x==0)
+		return 1;
+	else
+		f=x*fact(x-1);//Recursion
+	return f;
+}
